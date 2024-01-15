0cdecf0b50cfda543b454d2dc5ff6c893377e8cc
imkdw <imkdw@kakao.com>
2024-01-15 23:02:57 +0900

    Clerk를 활용한 SignIn, SignUp 연동

---
+++ twitch-clone/app/(auth)/layout.tsx
@@ -0,0 +1,7 @@
+interface Props {
+  children: React.ReactNode;
+}
+
+export default function AuthLayout({ children }: Props) {
+  return <div className="h-full flex items-center justify-center">{children}</div>;
+}
